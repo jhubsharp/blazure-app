a7be3e8e0feeb172791afc83b2a15019010ce3f0
Shawn Hubbard <shawn.hubbard@hubsharp.com>
2020-12-05 16:16:31 -0600

    Moving weather forecast to API call

--- Blazure.Web/Program.cs
+++ Blazure.Web/Program.cs
@@ -1,11 +1,6 @@
+using Blazure.Web.ServiceClients;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Blazure.Web
@@ -17,7 +12,8 @@ namespace Blazure.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.Configure<WeatherForecastOptions>(builder.Configuration.GetSection(WeatherForecastOptions.WeatherForecastConfig));
+            builder.Services.AddHttpClient<WeatherForecastClient>();
 
             await builder.Build().RunAsync();
         }
---
+++ Blazure.Web/ServiceClients/WeatherForecastClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static Blazure.Web.Pages.FetchData;
+
+namespace Blazure.Web.ServiceClients
+{
+    public class WeatherForecastClient
+    {
+        private HttpClient _client;
+
+        public WeatherForecastClient(HttpClient client, IOptions<WeatherForecastOptions> options)
+        {
+            _client = client;
+            _client.BaseAddress = new Uri(options.Value.BaseAddress);
+        }
+
+        public async Task<WeatherForecast[]> GetForecastAsync()
+        {
+            return await _client.GetFromJsonAsync<WeatherForecast[]>("weatherforecast");
+        }
+    }
+
+    public class WeatherForecastOptions
+    {
+        public const string WeatherForecastConfig = "api:weatherForecast";
+        
+        public string BaseAddress { get; set; }
+    }
+}
